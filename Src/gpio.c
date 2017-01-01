077a53857e73eb64324b4bc9dc53867f951286ae
Yuki-PC <yuqlid@gmail.com>
2017-01-02 06:55:10 +0900

    ボタンのGPIO入力修正

--- Src/gpio.c
+++ Src/gpio.c
@@ -114,7 +114,7 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pin : PtPin */
   GPIO_InitStruct.Pin = B1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PtPin */
